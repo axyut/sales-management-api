345210c679a28792920359bc0aeaf44430be046f
Achyut <aoogleak@gmail.com>
2023-07-05 08:21:05 +0545

    postgres db with supabase configured 1.1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // allow connection to frontend
+  //app.enableCors();
+
+  // Server
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () =>
+    console.log(`Server started at http://localhost:${PORT}`),
+  );
 }
-bootstrap();
+start();
---
+++ src/utils/connection.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(<string>process.env.PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    };
+  }
+}
